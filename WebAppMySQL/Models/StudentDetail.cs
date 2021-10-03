c829e23dc74025d7747bf36f9d5db29dc276dd46
Varun Barve <varun7691@gmail.com>
2021-10-03 17:38:55 +0530

    Adding Student Details

--- WebAppMySQL/Models/StudentDetail.cs
+++ WebAppMySQL/Models/StudentDetail.cs
@@ -1,10 +1,25 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace WebAppMySQL.Models
 {
     public class StudentDetail
     {
-        public StudentDetail()
-        {
-        }
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+
+        [Required]
+        [Column(TypeName = "nvarchar(100)")]
+        public string Name { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(11)")]
+        public string Phone_Number { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(10)")]
+        public string Birth_Date { get; set; }
     }
 }
